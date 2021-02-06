53240ccbe670be2ab78ffeee4d79a591e0887a19
Adenosie <adenosiez@gmail.com>
2021-02-06 23:50:04 +0900

    Implement get

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,11 @@
 use std::future::Future;
 use std::error::Error;
 use tokio::task::JoinHandle;
-use hyper::Body;
-use hyper::client::conn::{Builder, SendRequest};
-use hyper::{Request, Response};
 use tokio::net::TcpStream;
 use tokio_native_tls::{TlsConnector, native_tls};
+use hyper::http;
+use hyper::{Body, Request, Response};
+use hyper::client::conn::{Builder, SendRequest};
 
 use crate::detour::Detour;
 
@@ -20,13 +20,10 @@ impl Client {
         -> impl Future<Output = Result<Client, Box<dyn Error>>> {
         let host = String::from(host);
         async move {
-            // FIXME: why it doesn't work?
             let tls = native_tls::TlsConnector::new()?;
             let tls = TlsConnector::from(tls);
 
             let sock = TcpStream::connect((host.as_str(), 443)).await?;
-            
-            // an error occurs HERE when bypassing
             let sock = tls.connect(host.as_str(), Detour::new(sock)).await?;
     
             let (sender, conn) = Builder::new()
@@ -49,9 +46,20 @@ impl Client {
         }
     }
 
-    fn send_request(&mut self, req: Request<Body>)
+    pub fn send_request(&mut self, req: Request<Body>)
         -> impl Future<Output = hyper::Result<Response<Body>>> {
         self.sender.send_request(req)
+    }
+
+    pub fn get(&mut self, path: &str)
+        -> http::Result<impl Future<Output = hyper::Result<Response<Body>>>> {
+        let req = Request::builder()
+            .method("GET")
+            .uri(path)
+            .header("Host", &self.host)
+            .body(Body::from(""))?;
+
+        Ok(self.send_request(req))
     }
 }
 
@@ -59,31 +67,31 @@ impl Client {
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn conn_google() {
-        let mut client = Client::new("google.com").await.unwrap();
-        
-        let req = Request::builder()
-            .method("GET")
-            .header("Host", "google.com")
-            .body(Body::from(""))
-            .unwrap();
+    use std::error::Error;
+    async fn conn_client(host: &str) -> Result<(), Box<dyn Error>> {
+        let mut client = Client::new(host).await?;
+        let res = client.get("/")?.await?;
 
-        let res = client.send_request(req).await.unwrap();
-        println!("google.com: {}", res.status());
+        let status = res.status();
+        println!("{}: {}", host, status);
+        assert!(!status.is_client_error());
+
+        Ok(())
     }
 
     #[tokio::test]
-    async fn conn_hitomi() {
-        let mut client = Client::new("hitomi.la").await.unwrap();
-        
-        let req = Request::builder()
-            .method("GET")
-            .header("Host", "hitomi.la")
-            .body(Body::from(""))
-            .unwrap();
+    async fn conn_unblocked() {
+        conn_client("google.com").await.unwrap();
+        conn_client("example.com").await.unwrap();
+        conn_client("naver.com").await.unwrap();
+        conn_client("knowhow.or.kr").await.unwrap();
+    }
 
-        let res = client.send_request(req).await.unwrap();
-        println!("hitomi.la: {}", res.status());
+    #[tokio::test]
+    async fn conn_blocked() {
+        conn_client("e-hentai.org").await.unwrap();
+        conn_client("hitomi.la").await.unwrap();
+        conn_client("xvideos.com").await.unwrap();
+        conn_client("manatoki.net").await.unwrap();
     }
 }
